6795258d587d4bb1298c5b8120c8bddfc5bd46ed
Carlos Trinidad <carlos@carlostrinidad.me>
2020-06-11 22:26:57 -0300

    Agregado dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require('./app');
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 server.listen(port, function () {
     console.log(`El servidor esta funcionando en el puerto: ${port}`);
