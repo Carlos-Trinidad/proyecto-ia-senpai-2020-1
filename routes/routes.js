c12efe2f75468cb850c5196e3712f24d0dab2c8e
Carlos Trinidad <carlos@carlostrinidad.me>
2020-06-23 22:42:33 -0300

    Fix bug

--- routes/routes.js
+++ routes/routes.js
@@ -1,39 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const pingController = require('./../controllers/pingController');
-const watsonAssistantController = require('./../controllers/watsonAssistantController');
+const pingController = require("./../controllers/pingController");
+const watsonAssistantController = require("./../controllers/watsonAssistantController");
 
-router.get('/hola-mundo', async (req, res) => {2
-    console.log(req.query);
+router.get("/hola-mundo", async (req, res) => {
+  2;
+  console.log(req.query);
 
-    //let { usuario, foto } = req.query;
+  //let { usuario, foto } = req.query;
 
-    let usuario = req.query.usuario;
-    let foto = req.query.foto;
+  let usuario = req.query.usuario;
+  let foto = req.query.foto;
 
-    res.status(200).send({data: 'hola mundo usuario ' + usuario + ' con ip ' + req.ip + '. Veo que estas buscando la foto: ' + foto});
+  res
+    .status(200)
+    .send({
+      data:
+        "hola mundo usuario " +
+        usuario +
+        " con ip " +
+        req.ip +
+        ". Veo que estas buscando la foto: " +
+        foto,
+    });
 });
 
 // Rutas Ping
-router.get('/ping', pingController.getPing);
-router.post('/ping', pingController.postPing);
+router.get("/ping", pingController.getPing);
+router.post("/ping", pingController.postPing);
 
 // Rutas Watson Assistant
-router.get('/message', watsonAssistantController.sendMessage);
+router.get("/message", watsonAssistantController.sendMessage);
 
 // Rutas Webhook
-router.post('/webhook/assistant', async (req, res) => {
-  if(req.body.action === 'hello_world'){
-    return { message: 'Hello World' };
-}
-else if(req.body.action === 'hoteles_punta'){
-    return { hoteles: ['Conrad', 'Torreon']};
-}
-else{
-    return { message: 'World' };
-}
-})
+router.post("/webhook/assistant", async (req, res) => {
+  if (req.body.action === "hello_world") {
+    res.send({ message: "Hello World" });
+  } else if (req.body.action === "hoteles_punta") {
+    res.send({ hoteles: ["Conrad", "Torreon"] });
+  } else {
+    res.send({ message: "World" });
+  }
+});
 
 /*
 
@@ -48,6 +57,5 @@ router
 
   })
   .post(); */
-
 
 module.exports = router;
